0a4fca49f64b6c49282890e45f5d65467a1c0fd4
matheusquintaes <matheusquintaes@LAPTOP-L5I3F2FI.localdomain>
2020-03-05 16:01:21 +1300

    Contact page

---
+++ src/components/Contact/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import { Wrapper, Content, WorksContainer, WorkItem} from "./styled"
+
+
+const Contact= () => (
+    <>
+        <Wrapper>
+            <Content>
+                <h1>Contact</h1>
+
+                <p>You can connect with me via <a href="https://twitter.com/">Twitter</a>  and <a href="https://linkedin.com/">Linkedin</a> .</p>
+                <p>If you like to look at good photos, <a href="https://instagram.com/">Instagram</a></p>
+        
+            </Content>
+        </Wrapper>
+    </> 
+)
+
+export default Contact
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,11 +2,14 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import Contact from "../components/Contact"
+
 
 
 const ContactPage = () => (
     <Layout>
         <SEO title="Work" />
+        <Contact/>
     </Layout>
 )
 
