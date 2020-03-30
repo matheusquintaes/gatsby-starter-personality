1224fa8ae392d70ec470cc1190cd6030effbdf02
matheusquintaes <matheusquintaes@LAPTOP-L5I3F2FI.localdomain>
2020-03-30 19:31:46 +1300

    Fixing the text

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -15,7 +15,7 @@ const Intro = () => (
           <h3>DIGITAL DESIGNER - NEW ZEALAND</h3>
         </header>
         <p>A free, fully responsive gatsby starter template designed by <a target="blank"  href="https://www.twitter.com/matheusquintaes">@matheusquintaes</a> </p>
-        <p>You can learn more about the starters <a target="blank" href="https://www.gatsbyjs.org/docs/starters/">documentation here</a></p>
+        <p>You can learn more <a target="blank" href="https://www.gatsbyjs.org/docs/starters/"> about the starters here</a></p>
       </IntroContainer>
     </Wrapper>
     </> 
